b261275c49fa27a4999256deb30086bcc6cf5b24
waqas rafiq <wm66179@gmail.com>
2023-07-07 20:29:55 +0500

    add create polling fucntion and some debugging

--- Polls App/script.js
+++ Polls App/script.js
@@ -1,37 +1,67 @@
+
 const addButton = document.getElementById("addButton");
-const createPoll = document.querySelector("create");
-const userQuestion = document.querySelector("#userQ").value; 
+const createPoll = document.getElementById("Create");
+const input = document.querySelector("#userQ");
+const result = document.querySelector("#Question");
 const inputFirst = document.getElementById("input");
 const deleteInput = document.getElementById("dltBtn");
+const userInputContainer = document.getElementById("userinput");
  
-console.log(inputFirst)
-addButton.addEventListener("click", (e) => {
-    e.preventDefault();
-  const newInput = document.createElement("input");
-  input.type = "text";
-  newInput.setAttribute("class", "input");
-  newInput.setAttribute("placeholder", "Add");
-    console.log(newInput)
-  inputFirst.parentElement.appendChild(newInput);
+console.log(inputFirst);
 
-  const deleteButton = document.createElement("img");
-  deleteButton.setAttribute("src", "./Img/x-circle.svg");
-  deleteButton.setAttribute("class", "dltBtn");
-  deleteButton.addEventListener("click", () => {
-    inputFirst.parentElement.removeChild(newInput);
-    inputFirst.parentElement.removeChild(deleteButton);
-  });
-  inputFirst.parentElement.appendChild(deleteButton);
+const userInputs = [];
+
+addButton.addEventListener("click", (e) => {
+  e.preventDefault();
+const newInput = document.createElement("input");
+input.type = "text";
+newInput.setAttribute("class", "input");
+newInput.setAttribute("placeholder", "Add");
+  console.log(newInput)
+inputFirst.parentElement.appendChild(newInput);
+
+const deleteButton = document.createElement("img");
+deleteButton.setAttribute("src", "../Img/x-circle.svg");
+deleteButton.setAttribute("class", "dltBtn");
+deleteButton.addEventListener("click", () => {
+  inputFirst.parentElement.removeChild(newInput);
+  inputFirst.parentElement.removeChild(deleteButton);
+});
+inputFirst.parentElement.appendChild(deleteButton);
 
 });
 
-createPoll.addEventListener("click",(e) => {
-    e.preventDefault();
-    document.querySelector(".Question").innerHTML = `userQuestion`;
-    document.querySelector("#userQ").style.display = "none";
-    document.querySelector(".input").style.display = "none";
-    // document.querySelector("#userQ").style.display = "none";
+createPoll.addEventListener("click", (e) => {
+  e.preventDefault();
+  result.textContent = input.value;
+  const optionInputs = document.querySelectorAll(".input");
+  optionInputs.forEach((optionInput) => {
+    userInputs.push(optionInput.value);
+  });
 
+  displayUserInputs();
+  document.querySelector(".poll").style.display = "none";
+  document.querySelector(".pollAnswer").style.display = "block";
+});
+
+
+function displayUserInputs() {
+  userInputContainer.innerHTML = "";
+
+  for (let i = 0; i < userInputs.length; i++) {
+    const optionDiv = document.createElement("div");
+    optionDiv.setAttribute("class", "option");
+
+    const userInputElement = document.createElement("p");
+    userInputElement.textContent = userInputs[i];
+
+    const progressBar = document.createElement("progress");
+    progressBar.setAttribute("id", "file");
+    progressBar.setAttribute("max", "100");
+    progressBar.setAttribute("value", "50");
+
+    optionDiv.appendChild(userInputElement);
+    optionDiv.appendChild(progressBar);
+    userInputContainer.appendChild(optionDiv);
+  }
 }
-
-)
