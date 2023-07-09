8e098ad42a09fa12cf165c93a50fa2b4cd575e78
waqas rafiq <wm66179@gmail.com>
2023-07-10 03:32:10 +0500

    final

--- Polls App/modal.js
+++ Polls App/modal.js
@@ -1,78 +1,190 @@
-// Create button click event handler
-document.getElementById("createButtonM").addEventListener("click", function() {
-    var userQuestion = document.getElementById("userQM").value;
-    var options = document.getElementsByClassName("Minput");
-    var resultsContainer = document.getElementById("resultsContainer");
 
-    // Create a new response container
-    var responseContainer = document.createElement("div");
+const firebaseApp = firebase.initializeApp(
+    {
+      apiKey: "AIzaSyCzZTDuR5mwE-2Wq6D8-cK3lDnfpAkecbw",
+      authDomain: "real-time-polling-app.firebaseapp.com",
+      databaseURL: "https://real-time-polling-app-default-rtdb.firebaseio.com",
+      projectId: "real-time-polling-app",
+      storageBucket: "real-time-polling-app.appspot.com",
+      messagingSenderId: "151148454470",
+      appId: "1:151148454470:web:d1e8eaa2c32102e4f36047"
+    });
+    let database = firebase.database();
+
+
+document.getElementById("createButtonM").addEventListener("click", function() {
+    let userQuestion = document.getElementById("userQM").value;
+    let options = document.getElementsByClassName("Minput");
+    let resultsContainer = document.getElementById("resultsContainer");
+
+    let responseContainer = document.createElement("div");
     responseContainer.classList.add("Pcard");
 
-    // Create the question paragraph
-    var questionParagraph = document.createElement("p");
+    let questionParagraph = document.createElement("p");
     questionParagraph.textContent = userQuestion;
     questionParagraph.classList.add("ques");
     responseContainer.appendChild(questionParagraph);
 
-    // Create the options container
-    var optionsContainer = document.createElement("div");
+    let optionsContainer = document.createElement("div");
     optionsContainer.classList.add("option");
 
-    // Add the progress bars and progress texts for each option
-    for (var i = 0; i < options.length; i++) {
-        var option = options[i].value;
+    for (let i = 0; i < options.length; i++) {
+        let option = options[i].value;
 
-        // Create the option container
-        var optionContainer = document.createElement("div");
+        let optionContainer = document.createElement("div");
         optionContainer.classList.add("option-item");
 
-        // Create the option text
-        var optionText = document.createElement("p");
+        let optionText = document.createElement("p");
         optionText.textContent = option;
         optionContainer.appendChild(optionText);
 
-        // Create the progress bar
-        var progressBar = document.createElement("progress");
+        let progressBar = document.createElement("progress");
         progressBar.setAttribute("id", "file" + i);
         progressBar.setAttribute("id", "Ppro");
         progressBar.setAttribute("max", "100");
         progressBar.setAttribute("value", "0");
 
-        // Create the progress text
-        var progressText = document.createElement("span");
+        let progressText = document.createElement("span");
         progressText.setAttribute("class", "progress-text");
         progressText.textContent = "0%";
 
-        // Add event listener to each progress bar
         progressBar.addEventListener("click", (function(progressBar, progressText) {
             return function() {
-                var currentValue = Number(progressBar.value) + 1; // Increment the current value by 1
+                let currentValue = Number(progressBar.value) + 1; // Increment the current value by 1
                 progressBar.value = currentValue; // Update the progress bar value
                 progressText.textContent = currentValue + "%"; // Update the progress text
             };
         })(progressBar, progressText));
 
-        // Append the progress bar and progress text to the option container
         optionContainer.appendChild(progressBar);
         optionContainer.appendChild(progressText);
 
-        // Append the option container to the options container
         optionsContainer.appendChild(optionContainer);
     }
 
-    // Append the options container to the response container
     responseContainer.appendChild(optionsContainer);
 
-    // Append the new response to the results container
     resultsContainer.appendChild(responseContainer);
 
-    // Clear the input fields
+    
+    let pollData = {
+        question: userQuestion,
+        options: []
+    };
+console.log("poll data", pollData)
+    for (let i = 0; i < options.length; i++) {
+        let option = options[i].value;
+        let optionData = {
+            text: option,
+            votes: 0
+        };
+        pollData.options.push(optionData);
+    }
+
+    let newPollRef = database.ref("polls").push();
+    let pollId = newPollRef.key;
+
+    newPollRef.set(pollData);
+
     document.getElementById("userQM").value = "";
-    for (var i = 0; i < options.length; i++) {
+    for (let i = 0; i < options.length; i++) {
         options[i].value = "";
     }
-        // Close the modal
         let modal = document.getElementById("exampleModal");
         let modalInstance = bootstrap.Modal.getInstance(modal);
         modalInstance.hide();
 });
+
+
+const pollsRef = database.ref("polls");
+
+function displayPollData(pollId, pollData) {
+  let responseContainer = document.getElementById(pollId);
+
+  if (responseContainer) {
+    let optionsContainer = responseContainer.querySelector(".option");
+
+  let existingContainers = document.querySelectorAll(".Pcard");
+  existingContainers.forEach(function(container) {
+    if (container.id !== pollId) {
+      container.remove();
+    }
+  });
+    for (let i = 0; i < pollData.options.length; i++) {
+      let option = pollData.options[i];
+      let progressBar = optionsContainer.querySelector("#file" + i);
+      let progressText = optionsContainer.querySelector(".progress-text");
+
+      progressBar.value = option.votes; 
+      progressText.textContent = option.votes + "%";
+    }
+  } else {
+    let responseContainer = document.createElement("div");
+    responseContainer.classList.add("Pcard");
+    responseContainer.id = pollId;
+
+    let questionParagraph = document.createElement("p");
+    questionParagraph.textContent = pollData.question;
+    questionParagraph.classList.add("ques");
+    responseContainer.appendChild(questionParagraph);
+
+    let optionsContainer = document.createElement("div");
+    optionsContainer.classList.add("option");
+
+    for (let i = 0; i < pollData.options.length; i++) {
+      let option = pollData.options[i];
+
+      let optionContainer = document.createElement("div");
+      optionContainer.classList.add("option-item");
+
+      let optionText = document.createElement("p");
+      optionText.textContent = option.text;
+      optionContainer.appendChild(optionText);
+
+      let progressBar = document.createElement("progress");
+      progressBar.setAttribute("id", "file" + i);
+      progressBar.setAttribute("class", "Ppro");
+      progressBar.setAttribute("max", "100");
+      progressBar.setAttribute("value", option.votes);
+
+      let progressText = document.createElement("span");
+      progressText.setAttribute("class", "progress-text");
+      progressText.textContent = option.votes + "%";
+
+      progressBar.addEventListener("click", function () {
+        let currentValue = Number(progressBar.value) + 1; // Increment the current value by 1
+        progressBar.value = currentValue; // Update the progress bar value
+        progressText.textContent = currentValue + "%"; // Update the progress text
+
+        pollsRef
+          .child(pollId)
+          .child("options")
+          .child(i)
+          .child("votes")
+          .set(currentValue);
+      });
+
+      optionContainer.appendChild(progressBar);
+      optionContainer.appendChild(progressText);
+
+      optionsContainer.appendChild(optionContainer);
+    }
+
+    responseContainer.appendChild(optionsContainer);
+
+    let resultsContainer = document.getElementById("resultsContainer");
+    resultsContainer.appendChild(responseContainer);
+  }
+}
+
+function retrieveAndDisplayPollData() {
+  pollsRef.on("value", function (snapshot) {
+    snapshot.forEach(function (childSnapshot) {
+      let pollId = childSnapshot.key;
+      let pollData = childSnapshot.val();
+      displayPollData(pollId, pollData);
+    });
+  });
+}
+
+window.addEventListener("load", retrieveAndDisplayPollData);
--- Polls App/script.js
+++ Polls App/script.js
@@ -11,97 +11,29 @@ console.log(inputFirst);
 
 const userInputs = [];
 
-addButton.addEventListener("click", (e) => {
-  e.preventDefault();
-const newInput = document.createElement("input");
-input.type = "text";
-newInput.setAttribute("class", "input");
-newInput.setAttribute("placeholder", "Add");
-  console.log(newInput)
-inputFirst.parentElement.appendChild(newInput);
-
-const deleteButton = document.createElement("img");
-deleteButton.setAttribute("src", "../Img/x-circle.svg");
-deleteButton.setAttribute("class", "dltBtn");
-deleteButton.addEventListener("click", () => {
-  inputFirst.parentElement.removeChild(newInput);
-  inputFirst.parentElement.removeChild(deleteButton);
-});
-inputFirst.parentElement.appendChild(deleteButton);
-
-});
-
-createPoll.addEventListener("click", (e) => {
-  e.preventDefault();
-  result.textContent = input.value;
-  const optionInputs = document.querySelectorAll(".input");
-  optionInputs.forEach((optionInput) => {
-    userInputs.push(optionInput.value);
-  });
-
-  displayUserInputs();
-  document.querySelector(".Qcard").style.display = "none";
-  document.querySelector(".Pcard").style.display = "block";
-  document.querySelector(".Qmodal").style.display = "block";
-});
-
-
-function displayUserInputs() {
-  userInputContainer.innerHTML = "";
-
-  for (let i = 0; i < userInputs.length; i++) {
-    const optionDiv = document.createElement("div");
-    optionDiv.setAttribute("class", "option");
-
-    const userInputElement = document.createElement("p");
-    userInputElement.textContent = userInputs[i];
-    optionDiv.appendChild(userInputElement);
-
-    // const clicksData = "clickedValue"
-    const progressBar = document.createElement("progress");
-    progressBar.setAttribute("id", "file");
-    progressBar.setAttribute("max", "100");
-    progressBar.setAttribute("value", "0");
-    const progressText = document.createElement("span");
-    progressText.setAttribute("class", "progress-text");
-    progressText.textContent = "0%";
-
-    progressBar.addEventListener("click", () => {
-      let currentValue = progressBar.value;
-      currentValue = Number(currentValue) + 1; // Increment the current value by 1
-      progressBar.value = currentValue; // Update the progress bar value
-      progressText.textContent = `${currentValue}%`; // Update the progress text
-    });
-
-    optionDiv.appendChild(progressBar);
-    optionDiv.appendChild(progressText);
-    
-    userInputContainer.appendChild(optionDiv);
-  }
-}
 
 document.getElementById("addButtonM").addEventListener("click", () => {
   var optionsContainer = document.getElementById("optionsContainer");
 
-  // Create a new option div
   var optionDiv = document.createElement("div");
   optionDiv.classList.add("option");
 
-  // Create the input field
   var input = document.createElement("input");
   input.classList.add("Minput");
   input.type = "text";
   input.placeholder = "Add";
   input.required = true;
 
-  // Create the delete button
   var deleteButton = document.createElement("span");
   deleteButton.innerHTML = '<img class="dltBtn" src="../Img/x-circle.svg" alt="">';
 
-  // Add the input field and delete button to the option div
+  deleteButton.addEventListener("click", () => {
+    optionDiv.parentNode.removeChild(optionDiv);
+  });
+
+
   optionDiv.appendChild(input);
   optionDiv.appendChild(deleteButton);
 
-  // Add the option div to the options container
   optionsContainer.appendChild(optionDiv);
 });
